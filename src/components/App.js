945a507ae91ed7f43a3ad65bbac84e7d73f0bcc8
Chris <chriswndegwa@gmail.com>
2024-11-18 13:55:52 +0300

    final

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,8 @@
+// src/components/App.js
 import React from "react";
+import NavBar from "./NavBar";  // Import NavBar component
+import Home from "./Home";      // Import Home component
+import About from "./About";    // Import About component
 
 function App() {
   return (
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,15 @@
+// src/components/Home.js
 import React from "react";
+import { username, city } from "../data/user";  // Named imports
 
 function Home() {
   return (
     <div id="home">
-      <h1>
-        {username} is a Web Developer from {city}
-      </h1>
+      <h1>Home</h1>
+      <p>{username}</p>
+      <p>{city}</p>
     </div>
   );
 }
+
+export default Home;
